20867d77c8a5a3110185cc8a61e7ef4e645c581e
jirokk <kakaijiro@gmail.com>
2025-02-20 21:44:01 +0900

    Refactor product types and validation schema; enhance ProductCard and ProductList components

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,4 @@
 import { clsx, type ClassValue } from "clsx";
-import { json } from "stream/consumers";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -9,4 +8,10 @@ export function cn(...inputs: ClassValue[]) {
 // convert prisma object into a regular js object
 export function convertToPlainObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
+}
+
+// format number with decimal places
+export function formatNumberWithDecimal(num: number): string {
+  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
 }
